3f96397c75b2a44de9d6d3d4683f43aa79fe7750
Johan <johan6119@gmail.com>
2024-05-15 17:27:07 -0500

    agregando visualizacion de rol en fullcalendar

--- app/Livewire/Asistencia/MisMarcaciones.php
+++ app/Livewire/Asistencia/MisMarcaciones.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Asistencia;
 
 use App\Models\Empleado;
 use App\Models\MarcacionDetalle;
+use App\Models\RolEmpleado;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
@@ -15,11 +16,25 @@ class MisMarcaciones extends Component
         $this->tituloPagina = "Mis marcaciones";
     }
     function getMarcaciones() {
-        $empleado = Empleado::where('userId', Auth::user()->id)->first();
+        $userId = Auth::user()->id;
+        $empleado = Empleado::where('userId', $userId)->first();
         $marcaciones = MarcacionDetalle::select(DB::raw("DATE_FORMAT(marcacion, '%H:%i:%s') AS title"), 
                                                 DB::raw("DATE_FORMAT(marcacion, '%Y-%m-%d') AS start"))
                                             ->where('codigo', $empleado->codigo)->get()->toArray();
-        return $marcaciones;
+
+        $rol = RolEmpleado::select(DB::raw("CONCAT('Turno: ','',D.turno) as title"), 
+                            DB::raw("CONCAT(R.anio,'-',LPAD(R.mes,2,'0'),'-',LPAD(D.dia,2,'0')) AS start"),
+                            DB::raw("CONCAT('#229954') as color")
+                            )
+                            ->from('rol_personal_empleado AS E')
+                            ->join('rol_personal AS R', 'R.id', '=', 'E.rolId')
+                            ->join('rol_personal_detalle AS D', 'D.rolEmpleadoId', '=', 'E.id')
+                            ->where('E.empleadoId', $empleado->id)
+                            ->where('R.estadoId', 2) //solo rol aprobado
+                            ->get()->toArray();
+        $data = array_merge($marcaciones, $rol);
+        
+        return $data;
     }
     public function render()
     {
