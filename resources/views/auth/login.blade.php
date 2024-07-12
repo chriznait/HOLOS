d5cd5a67c6c0f53faf18d0086530fbe11ae47897
christian jordy mamani sucasaire <chriz_nait@hotmail.com>
2024-07-12 14:46:04 -0500

    agregar mas funcionalidades al inicio de HOLOS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
                         <!-- Login-->
                         <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
                             <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-                                <h2 class="card-title fw-bold mb-1">Bienvenido a {{ config('app.name', 'Laravel') }}</h2>
+                                <h2 class="card-title fw-bold mb-1">Bienvenido a {{ config('app.name', 'HOLOS') }}</h2>
                                 @if ($errors->any())
                                 <div class="alert alert-danger" role="alert">
                                     <div class="alert-body">
